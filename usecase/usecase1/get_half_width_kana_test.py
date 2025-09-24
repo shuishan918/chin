b06ea5821fee907f8db0c493d41ea29b3ed18e48
shuishan918 <yingyun3@gmail.com>
2025-09-24 16:19:02 +0900

    Delete usecase/usecase1 directory

--- usecase/usecase1/get_half_width_kana_test.py
+++
@@ -1,7 +0,0 @@
-from get_half_width_kana import convert_to_halfwidth_kana
-
-jp_db_name = input("DB名称を入力してください: ").strip()
-if jp_db_name:
-    results = convert_to_halfwidth_kana(jp_db_name)
-    if results:
-        print(f"{results} ")
