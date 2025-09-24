445bbde4b5e9267a44457acbe0538bd9cbecf87f
shuishan918 <yingyun3@gmail.com>
2025-09-24 16:20:00 +0900

    Delete usecase5 directory

--- usecase5/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export WXO_SECURITY_SCHEMA_sj_poc="key_value_creds"
-export WXO_CONNECTION_sj_poc_ibm_cos_endpoint="https://s3.us-south.cloud-object-storage.appdomain.cloud"
-export WXO_CONNECTION_sj_poc_ibm_api_key="YBS99RLOjuf225NESbdiISGenv9mMjyWs8tZmcbBcYpKl"
-export WXO_CONNECTION_sj_poc_ibm_auth_endpoint="https://iam.cloud.ibm.com/identity/token"
--- usecase5/get_pgmid_and_ethics_db_names_test.py
+++
@@ -1,7 +0,0 @@
-from usecase5.uc5_1_get_pgmid_from_jp_sys_name import uc5_1_get_pgmid_from_jp_sys_name
-from usecase5.uc5_2_get_pgmid_and_ethics_db_names_sum import uc5_2_get_pgmid_and_ethics_db_names_sum
-
-pgmid = uc5_1_get_pgmid_from_jp_sys_name("ﾀﾞｲﾘﾃﾝ ｵﾝﾗｲﾝ")
-print(f"pgmidは：{pgmid}")
-ethics_db_names = uc5_2_get_pgmid_and_ethics_db_names_sum("DVT0214","HC129XXX")
-print(f"pgmid_and_ethics_db_names:{ethics_db_names}")
